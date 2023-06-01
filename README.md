b3741de7e010b5b4bff1998a35b991528a3a73cc
FrankLin23 <linzhangsheng23@163.com>
2023-06-01 22:49:00 +0800

    feat: add dark mode

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - Astro
 - UnoCSS
+
+## Development Plan
+- [ ] Dark Mode
+- [ ] Blog Page
+- [ ] Resume Page
+- [ ] Project Page
--- unocss.config.ts
+++ unocss.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     }),
     presetUno(),
     presetMini({
-      dark: "media",
+      dark: "class",
     }),
   ],
 });
